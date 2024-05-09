14d98a2f4dda81d7bc5bd023fc82540ad5b62ab6
Vishal Kumar <vishalkrdwiwedi@gmail.com>
2024-05-09 13:09:59 +0530

    axios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 
-import Practice from './Practice';
-import Parent from './Useprops/Parent';
-import Newproduct from './components/Newproduct';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js'
+import Axios from './Practice/Axios';
 
 function App() {
   return (
     
     <>
-    <Newproduct/>
-    <Parent/>
-    <Practice/>
+    <Axios/>
     
     </>
   );
